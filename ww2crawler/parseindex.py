61e1b25b4a9aca6035590aa4b09898639a12a5df
Marcel Kerkveld <4930603+mke21@users.noreply.github.com>
2017-01-04 18:55:19 +0100

    can now parse text from every post and fixed bug in finding next

--- ww2crawler/parseindex.py
+++ ww2crawler/parseindex.py
@@ -3,6 +3,8 @@ Uses beautifulsoup to parse the stuff
 """
 from bs4 import BeautifulSoup
 
+from datetime import datetime
+
 
 class ParsePageBase(object):
     """
@@ -20,9 +22,10 @@ class ParsePageBase(object):
         gets url of next page, returns none if last
         content must contain 'Next'
         """
-        a = self.soup.find('a', class_='text')
-        if 'Next' in a.contents[0]:
-            return a['href']
+        all = self.soup.find_all('a', class_='text')
+        for a in all:
+            if 'Next' in a.contents[0]:
+                return a['href']
         else:
             return None
 
@@ -45,11 +48,35 @@ class ParseIndexPage(ParsePageBase):
             )
             
 
-class ParseThreadPage(object):
+class ParseThreadPage(ParsePageBase):
     """
     parses thread page 
     """
 
-    def __init__(self, text):
-        self.soup = BeautifulSoup(text, 'html.parser')
+    def convert_datetime(self, message):
+        m = message.find('span', class_='DateTime').string
+        return datetime.strptime(m, '%b %d, %Y')
+    
+    def get_text(self, message):
+        s = message.find('blockquote', class_ = 'messageText')
+        text = ''.join(
+            s.find_all(text=True, recursive=False)
+            ).replace(
+                '\n', ' '
+                )
         
+        return text
+        
+    def content(self):
+        """
+        will get all posts on page and return a dictionary with 
+        date, author, postno and textual content.
+        """
+        for message in self.soup.find_all('div', class_='messageInfo'):
+            yield {
+                'author': message.find('a', class_='author').string,
+                'postno': message.find('a', class_='postNumber').string,
+                'date': self.convert_datetime(message), # converts date
+                'text': self.get_text(message),
+                }
+                
