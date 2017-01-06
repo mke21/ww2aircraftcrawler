136fa4dff0499327287750466d4c13711607058c
Marcel Kerkveld <4930603+mke21@users.noreply.github.com>
2017-01-06 19:32:45 +0100

    added fighters module with regex from earlier script

--- ww2crawler/walksite.py
+++ ww2crawler/walksite.py
@@ -6,7 +6,12 @@ from .parseindex import ParseIndexPage, ParseThreadPage
 
 import requests
 
-def threat_urls(base, part):
+def walk_index(base, part):
+    """
+    walks a forum for url's
+    base = "https://ww2aircraft.net/forum/" mind the trailing '/'
+    part = rest of the forum adres
+    """
     url = base + part
     while url:
         page_content = requests.get(url).text
@@ -17,3 +22,23 @@ def threat_urls(base, part):
             url = base + parser.next
         except TypeError:
             break
+
+
+def walk_thread(base, part):
+    """
+    Walks thread and gets all posts
+    returns list of posts
+    """
+    url = base + part
+    result = []
+    while url:
+        page_content = requests.get(url).text
+        parser = ParseThreadPage(page_content)
+        result += [i for i in parser.content()]
+        try:
+            url = base + parser.next
+            print ( url)
+        except TypeError:
+            break
+    return result
+    
