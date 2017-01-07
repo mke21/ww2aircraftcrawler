ae931e9e7765e6838c33fda53e8d2ebed3ce6842
Marcel Kerkveld <4930603+mke21@users.noreply.github.com>
2017-01-07 16:22:56 +0100

    have to change name of analyse.py to iter post

---
+++ ww2crawler/analyse.py
@@ -0,0 +1,43 @@
+"""
+A way to iterate over all downloaded posts
+"""
+import os
+import json
+
+def file_iter(path):
+    """
+    Iterates over files and returns hard paths
+    Will go only 1 level deep
+    """
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            yield os.path.join(path, filename)
+
+
+def lcase(obj):
+    obj['text'] = obj['text'].lower()
+    return obj
+
+
+def read_files(path):
+    """
+    reads files and returns datastructure
+    path is dir in which jsons are saved
+    will lowercase all content
+    """
+    for f in file_iter(path):
+        yield map(
+            lcase,
+            json.load(
+                open(f)
+            )
+        )
+
+def readposts(path):
+    """
+    Yields single posts of all files
+    """
+    for posts in read_files(path):
+        for p on posts:
+            yield p
+            
